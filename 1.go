fb0797c8e949aa986244b4f1df2eeae4585d41e7
Alaneel <96646828+Alaneel@users.noreply.github.com>
2022-11-06 11:02:50 +0800

    initial commit

---
+++ 1.go
@@ -0,0 +1,11 @@
+// This is a comment
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/* The code below will print Hello world! */
+	fmt.Println("Hello world!")
+}
---
+++ 9.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		a int
+		b int    = 1
+		c string = "Hello"
+	)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
